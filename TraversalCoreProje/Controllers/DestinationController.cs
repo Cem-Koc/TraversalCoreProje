7c1613107c6a3c2ef56e64b2b732b681bce3a174
Cem Koç <cem-koc@outlook.com>
2023-07-23 20:22:42 +0300

    Yorum işlemleri

--- TraversalCoreProje/Controllers/DestinationController.cs
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -21,7 +21,9 @@ namespace TraversalCoreProje.Controllers
         [HttpGet]
         public IActionResult DestinationDetails(int id)
         {
-            return View();
+            ViewBag.i = id;
+            var values = destinationManager.TGetByID(id);
+            return View(values);
         }
         [HttpPost]
         public IActionResult DestinationDetails(Destination p)
