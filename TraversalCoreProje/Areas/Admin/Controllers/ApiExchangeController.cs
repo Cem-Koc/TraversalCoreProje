b5b443c283e299fa992d3e74141d6ce03122fc63
Cem Koç <cem-koc@outlook.com>
2023-09-18 21:23:29 +0300

    Booking Exchange Api

---
+++ TraversalCoreProje/Areas/Admin/Controllers/ApiExchangeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class ApiExchangeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            List<BookingExchangeViewModel2> bookingExchangeViewModels = new List<BookingExchangeViewModel2>();
+            //using System.Net.Http.Headers;
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/metadata/exchange-rates?currency=TRY&locale=en-gb"),
+                Headers =
+                {
+                    { "X-RapidAPI-Key", "986e67d901msh35ad00dfede23e9p1f40e2jsn4dabbf90b721" },
+                    { "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+                },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<BookingExchangeViewModel2>(body);
+                return View(values.exchange_rates);
+            }
+        }
+    }
+}
