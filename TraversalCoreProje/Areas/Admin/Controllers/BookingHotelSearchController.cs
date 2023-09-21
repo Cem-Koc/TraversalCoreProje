068838034dab70f20b8be7a3a7bd5c9def2314a9
Cem Koç <cem-koc@outlook.com>
2023-09-21 22:24:50 +0300

    CQRS Design Pattern

---
+++ TraversalCoreProje/Areas/Admin/Controllers/BookingHotelSearchController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[AllowAnonymous]
+    public class BookingHotelSearchController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+			//using System.Net.Http.Headers;
+			var client = new HttpClient();
+			var request = new HttpRequestMessage
+			{
+				Method = HttpMethod.Get,
+				RequestUri = new Uri("https://booking-com.p.rapidapi.com/v1/hotels/search?checkin_date=2023-09-27&dest_type=city&units=metric&checkout_date=2023-09-28&adults_number=2&order_by=popularity&dest_id=-1456928&filter_by_currency=EUR&locale=tr&room_number=1&children_number=2&children_ages=5%2C0&categories_filter_ids=class%3A%3A2%2Cclass%3A%3A4%2Cfree_cancellation%3A%3A1&page_number=0&include_adjacency=true"),
+				Headers =
+				{
+					{ "X-RapidAPI-Key", "986e67d901msh35ad00dfede23e9p1f40e2jsn4dabbf90b721" },
+					{ "X-RapidAPI-Host", "booking-com.p.rapidapi.com" },
+				},
+			};
+			using (var response = await client.SendAsync(request))
+			{
+				response.EnsureSuccessStatusCode();
+				var body = await response.Content.ReadAsStringAsync();
+				var bodyReplace = body.Replace(".", "");
+				var values = JsonConvert.DeserializeObject<BookingHotelSearchViewModel>(bodyReplace);
+				return View(values.result);
+			}
+        }
+    }
+}
