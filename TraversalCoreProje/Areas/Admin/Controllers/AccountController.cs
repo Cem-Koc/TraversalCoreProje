7c0fd0bec070a6ea2973f4a9a8b46b2f0ef5f5ef
Cem Koç <cem-koc@outlook.com>
2023-09-24 01:38:28 +0300

    Unit Of Work İmplementasyonu

---
+++ TraversalCoreProje/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Abstract.AbstractUow;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalCoreProje.Areas.Admin.Models;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        private readonly IAccountService _accountService;
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AccountViewModel model)
+        {
+            var valueSender = _accountService.TGetByID(model.SerderID);
+            var valueReceiver = _accountService.TGetByID(model.ReceiverID);
+
+            valueSender.Balance -= model.Amount;
+            valueReceiver.Balance += model.Amount;
+
+            List<Account> modifiedAccount = new List<Account>()
+            {
+                valueSender,
+                valueReceiver
+            };
+
+            _accountService.TMultiUpdate(modifiedAccount);
+
+            return View();
+        }
+    }
+}
---
+++ TraversalCoreProje/Areas/Admin/Models/AccountViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Admin.Models
+{
+    public class AccountViewModel
+    {
+        public int SerderID { get; set; }
+        public int ReceiverID { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
